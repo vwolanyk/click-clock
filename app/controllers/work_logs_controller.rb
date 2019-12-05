636c56fe90d00b1efbb1774c71933d24a2ed879f
Vlad Wolanyk <vladwolanyk@gmail.com>
2019-12-04 21:39:16 -0500

    parsed time to utc for good time measures

--- app/controllers/work_logs_controller.rb
+++ app/controllers/work_logs_controller.rb
@@ -79,13 +79,13 @@ class WorkLogsController < ApplicationController
     def parsed_start_time
       return false unless params[:work_log]['log_in_time(4i)'] != "" && params[:work_log]['log_in_time(5i)'] != ""
       time = "#{ params[:work_log]['log_in_time(4i)']}:#{params[:work_log]['log_in_time(5i)']}"
-      Time.strptime("#{parsed_date}:#{time}", '%d/%m/%Y:%H:%M')
+      Time.strptime("#{parsed_date}:#{time}", '%d/%m/%Y:%H:%M').utc
     end
 
     def parsed_end_time
       return false unless params[:work_log]['log_out_time(4i)'] != "" && params[:work_log]['log_out_time(5i)'] != ""
       time = "#{ params[:work_log]['log_out_time(4i)']}:#{params[:work_log]['log_out_time(5i)']}"
-      Time.strptime("#{parsed_date}:#{time}", '%d/%m/%Y:%H:%M')
+      Time.strptime("#{parsed_date}:#{time}", '%d/%m/%Y:%H:%M').utc
     end
 
     def parsed_date
